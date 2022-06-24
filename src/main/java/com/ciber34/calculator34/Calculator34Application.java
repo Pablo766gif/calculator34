6128eae63891672ec4a29f8b071744d3e6c785c1
Pablo766gif <pablotesu@gmail.com>
2022-06-24 22:26:42 +0200

    Quitando tabulados

--- src/main/java/com/ciber34/calculator34/Calculator34Application.java
+++ src/main/java/com/ciber34/calculator34/Calculator34Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator34Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Calculator34Application.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(Calculator34Application.class, args);
+        }
 
 }
