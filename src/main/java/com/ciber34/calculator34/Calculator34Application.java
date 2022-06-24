ceda8326901de8c1016ccbbaaa2edacd6c86f4a2
Pablo766gif <pablotesu@gmail.com>
2022-06-24 23:10:51 +0200

    Arreglando tabulados

--- src/main/java/com/ciber34/calculator34/Calculator34Application.java
+++ src/main/java/com/ciber34/calculator34/Calculator34Application.java
@@ -1,13 +1,10 @@
 package com.ciber34.calculator34;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Calculator34Application {
-
         public static void main(String[] args) {
                 SpringApplication.run(Calculator34Application.class, args);
         }
-
 }
